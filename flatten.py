6f35e3d8924b26ec5acfbd57d9bfe96f0b54516e
mschafer <mschafer@alum.mit.edu>
2019-01-28 21:48:35 -0500

    vectorize ode23

--- flatten.py
+++ flatten.py
@@ -43,3 +43,28 @@ class FlatEdge:
             tmp = surfaceNormal.crossProduct(tangent)
             curve_geo = curvature.dotProduct(tmp)
  
+    # rhs for ODE, state vector is (x, y, x_t, y_t) 
+    def rhs(self, t, state):
+        state_t = [state[2], state[3], 0., 0.]
+        edgeEval = self.edge.evaluator
+        faceEval = self.face.evaluator
+
+        (ret, p) = edgeEval.getPointAtParameter(t)
+        (ret, r_t) = edgeEval.getFirstDerivative(t)
+        (ret, r_tt) = edgeEval.getSecondDerivative(t)
+        (ret, tangent) = edgeEval.getTangent(t)
+        (ret, curvature, curveMag) = edgeEval.getCurvature(t)
+        ret = curvature.scaleBy(curveMag)
+        
+        (ret, sparam) = faceEval.getParameterAtPoint(p)
+        (ret, maxTangent, maxCurvature, minCurvature) = faceEval.getCurvature(sparam)
+        (ret, surfaceNormal) = faceEval.getNormalAtPoint(p)
+        tmp = surfaceNormal.crossProduct(tangent)
+        curve_geo = curvature.dotProduct(tmp)
+        
+        A = r_t.dotProduct(r_tt) / r_t.dotProduct(r_t)
+        B = curve_geo * r_t.length
+        state_t[2] = A * state[2] - B * state[3]
+        state_t[3] = A * state[3] + B * state[2]
+        return state_t
+         
--- ode23.py
+++ ode23.py
@@ -12,18 +12,32 @@ def ode23(f, x0):
     h = .1
     x = x0
     t = 0
+    
+    # first stage
     s1 = f(t, x)
     
-    xs1 = [h/2 * z for z in s1]
+    #stage 2: s2 = f(t+h/2, x + 1/2 h s1)
+    xs1 = [h/2. * z for z in s1]
     x1 = [a+b for a,b in zip(x,xs1)]
-    s2 = f(t+h/2, x1)
+    s2 = f(t+h/2., x1)
     
+    # stage 3: s3 = f(t+3h/4, x + 3/4 h s2)
+    xs2 = [3.*h/4. * z for z in s2]
+    x2 = [a+b for a,b in zip(x,xs2)]
+    s3 = f(t+3.*h/4., x2)
     
-    s3 = f(t+3*h/4, x + 3*h*s2/4)
-    x = x + h*(2*s1 + 3*s2 + 4*s3)/9
+    #output: t = t+h, x = x + h/9(2 s1 + 3 s2 + 4 s3)
     t = t + h
+    for idx in range(len(x)):
+        x[idx] = h / 9. * (2.*s1[idx] + 3*s2[idx] + 4*s3[idx])
+    
+    # s4 is same as s1 for next step    
     s4 = f(t, x)
-    e = h*(-5*s1 + 6*s2 + 8*s3 + -9*s4)/72
+
+    # error: h*(-5*s1 + 6*s2 + 8*s3 + -9*s4)/72
+    e = x
+    for idx in range(len(x)):
+        e[idx] = h / 72. * (-5.*s1[idx] + 6*s2[idx] + 8*s3[idx] - 9.*s4[idx])
     return e
     
 
