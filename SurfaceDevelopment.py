079ab69999877fd3bcbc6424d31b56eebd048409
Marc Schafer <mschafer@alum.mit.edu>
2020-01-17 21:37:36 -0500

    Update SurfaceDevelopment.py
    Create a new sketch on selected plane for flattened shape.
    
    Loop over all the selected faces.

--- SurfaceDevelopment.py
+++ SurfaceDevelopment.py
@@ -26,17 +26,24 @@ class FlattenCommandExecuteHandler(adsk.core.CommandEventHandler):
             command = args.firingEvent.sender
             inputs = command.commandInputs
 
-            input0 = inputs[0];     # faces to flatten
-            sel0 = input0.selection(0)
-            face = sel0.entity
-            loops = face.loops
-            outerLoop = loops[0]
-            fl = FlatLoop(outerLoop)
+            # faces to flatten
+            flattened = []
+            input0 = inputs[0]
+            for isel in range(input0.selectionCount):
+                facesel = input0.selection(isel)
+                face = facesel.entity
+                loops = face.loops
+                outerLoop = loops[0]
+                flattened.append(FlatLoop(outerLoop))
 
             input1 = inputs[1]     # sketch
             sel1 = input1.selection(0)
-            sketch = sel1.entity
-            addSketchSpline(fl, sketch)
+            plane = sel1.entity
+            root = design.rootComponent
+            sketch = root.sketches.add(plane)
+
+            for fl in flattened:
+                addSketchSpline(fl, sketch)
 
             #airfoil = Airfoil();
             #airfoil.Execute(sel0.entity, input1.value, input2.value, input3.value, input4.value);
@@ -93,8 +100,12 @@ class FlattenCommandCreatedHandler(adsk.core.CommandCreatedEventHandler):
             i0.setSelectionLimits(1, 0)
             i0.addSelectionFilter(adsk.core.SelectionCommandInput.Faces)
             
-            i1 = inputs.addSelectionInput('Sketch', 'Flattened Sketch', 'Sketch to add flattened faces to')
-            i1.addSelectionFilter(adsk.core.SelectionCommandInput.Sketches)
+            #i1 = inputs.addSelectionInput('Sketch', 'Flattened Sketch', 'Sketch to add flattened faces to')
+            #i1.addSelectionFilter(adsk.core.SelectionCommandInput.Sketches)
+
+            i1 = inputs.addSelectionInput('ConstPlane', 'Construction Plane', 'Please select a construction plane')
+            i1.addSelectionFilter(adsk.core.SelectionCommandInput.ConstructionPlanes)
+            i1.addSelectionFilter(adsk.core.SelectionCommandInput.RootComponents)
 
         except:
             if ui:
@@ -131,58 +142,12 @@ def run(context):
 
 
 def addSketchSpline(flatLoop, sketch):
-    normal = sketch.xDirection.crossProduct(sketch.yDirection)
-    normal.transformBy(sketch.transform)
-    origin = sketch.origin
-    origin.transformBy(sketch.transform)
-    rotationMatrix = None
-    translationMatrix = None
-    Xvector = adsk.core.Vector3D.create(1.0, 0.0, 0.0)   # X axis
-    Yvector = adsk.core.Vector3D.create(0.0, 1.0, 0.0)   # Y axis
-    Zvector = adsk.core.Vector3D.create(0.0, 0.0, 1.0)   # Z axis
-
-    if sketch.xDirection.isParallelTo(Xvector):
-        if sketch.yDirection.isParallelTo(Yvector):
-            # XY plane, inverted normal angles
-            # ui.messageBox('XY Plane')
-            rotationMatrix = adsk.core.Matrix3D.create()
-            rotationMatrix.setToRotation(math.radians(-AOI), normal, origin)
-            translationMatrix = adsk.core.Matrix3D.create()
-            translationMatrix.translation = adsk.core.Vector3D.create(OffsetX, OffsetY, 0.0)
-    if sketch.xDirection.isParallelTo(Zvector):
-        if sketch.yDirection.isParallelTo(Yvector):
-            # YZ plane, - 90 degrees
-            # ui.messageBox('YZ plane')
-            rotationMatrix = adsk.core.Matrix3D.create()
-            rotationMatrix.setToRotation(math.radians(-90.0), normal, origin)
-            translationMatrix = adsk.core.Matrix3D.create()
-            translationMatrix.translation = adsk.core.Vector3D.create(-OffsetY, OffsetX, 0.0)
-    if sketch.xDirection.isParallelTo(Xvector):
-        if sketch.yDirection.isParallelTo(Zvector):
-            # XZ plane, inverted normal angles
-            # ui.messageBox('XZ plane')
-            rotationMatrix = adsk.core.Matrix3D.create()
-            rotationMatrix.setToRotation(math.radians(-AOI), normal, origin)
-            translationMatrix = adsk.core.Matrix3D.create()
-            translationMatrix.translation = adsk.core.Vector3D.create(OffsetX, -OffsetY, 0.0)
-        
-#        translationMatrix = adsk.core.Matrix3D.create()
-#        translationMatrix.translation = adsk.core.Vector3D.create(OffsetX, OffsetY, 0.0)
-
-
     for fe in flatLoop.flatEdges:
         points = adsk.core.ObjectCollection.create()
         for pt in fe.points:
             x = pt[0]
             y = pt[1]
-
-            if sketch.xDirection.isParallelTo(Xvector):
-                if sketch.yDirection.isParallelTo(Zvector):
-                    # XZ plane, mirrored Y
-                    y = -1.0 * y
             point = adsk.core.Point3D.create(x, y, 0.0)
-            #point.transformBy(rotationMatrix)
-            #point.transformBy(translationMatrix)            
             points.add(point)
     
         sketch.sketchCurves.sketchFittedSplines.add(points)
